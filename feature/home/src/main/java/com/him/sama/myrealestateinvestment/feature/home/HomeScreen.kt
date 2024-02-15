c585877cc5674faafcbe5bdd0e9a146928035755
Abdulrohim 'Him' Sama <a.rohim.sama@gmail.com>
2024-02-15 18:31:51 +0700

    Added a few components for home screen

--- feature/home/src/main/java/com/him/sama/myrealestateinvestment/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/him/sama/myrealestateinvestment/feature/home/HomeScreen.kt
@@ -1,12 +1,15 @@
 package com.him.sama.myrealestateinvestment.feature.home
 
 import android.annotation.SuppressLint
+import android.hardware.lights.Light
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,11 +17,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -28,12 +36,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.him.sama.myrealestateinvestment.common.ui.R.drawable
 import com.him.sama.myrealestateinvestment.common.ui.theme.SpotifyJetpackCompose
-import com.him.sama.myrealestateinvestment.common.ui.theme.nautral_50
 
 @Composable
 fun HomeScreen() {
@@ -44,79 +51,143 @@ fun HomeScreen() {
 @Composable
 private fun Body() {
     Scaffold {
-        LazyColumn(
+        LazyVerticalGrid(
             modifier = Modifier
                 .fillMaxSize()
+                .background(color = Color.Black)
                 .graphicsLayer(clip = false),
-            contentPadding = PaddingValues(top = 24.dp, bottom = 56.dp)
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(top = 24.dp, bottom = 56.dp, start = 16.dp, end = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            item { Header() }
-            item { PileGraph() }
-            item { Spacer(modifier = Modifier.height(24.dp)) }
-            item { AssetsList(Modifier) }
-        }
-    }
-}
-
-@Composable
-private fun AssetsList(modifier: Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .background(Color.White, shape = RoundedCornerShape(topStart = 22.dp, topEnd = 22.dp))
-            .clip(RoundedCornerShape(topStart = 22.dp, topEnd = 22.dp))
-            .padding(16.dp)
-    ) {
-        Text(
-            text = "Properties",
-            style = MaterialTheme.typography.bodySmall,
-            color = nautral_50
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        repeat(100) {
-            AssetItem()
-        }
-    }
-}
-
-@Composable
-fun AssetItem() {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Spacer(modifier = Modifier.height(8.dp))
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Row(
-                modifier = Modifier.weight(1f)
-            ) {
-                Icon(
-                    painter = painterResource(id = drawable.ic_baseline_add_circle_24),
-                    contentDescription = "Asset icon",
-                )
-                Spacer(modifier = Modifier.width(4.dp))
-                Text(text = "Condo A Condo A Condo A Condo A Condo A Condo A")
+            item(span = { GridItemSpan(2) }) {
+                Header()
             }
-            Text(text = "2m", style = MaterialTheme.typography.bodyMedium)
+            item(span = { GridItemSpan(2) }) {
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+            items(8) {
+                RecommendationItem()
+            }
+            item(span = { GridItemSpan(2) }) {
+                Spacer(modifier = Modifier.height(26.dp))
+            }
+            item(span = { GridItemSpan(2) }) {
+                HighlightedAlbum()
+            }
+            item(span = { GridItemSpan(2) }) {
+                HighlightedAlbum()
+            }
+            item(span = { GridItemSpan(2) }) {
+                HighlightedAlbum()
+            }
         }
+    }
+}
+
+@Composable
+fun HighlightedAlbum() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(52.dp)
+                    .background(
+                        color = Color.LightGray,
+                        shape = CircleShape
+                    )
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                Text(
+                    text = "More like",
+                    style = MaterialTheme.typography.labelSmall,
+                    color = Color.LightGray
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = "Alan Walker",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = Color.LightGray
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .graphicsLayer(clip = false),
+            contentPadding = PaddingValues(top = 24.dp, bottom = 56.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(5) {
+                AlbumItem()
+            }
+        }
+    }
+}
+
+@Composable
+private fun AlbumItem() {
+    Column(
+        modifier = Modifier
+            .width(150.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Box(
+            modifier = Modifier
+                .size(150.dp)
+                .background(
+                    color = Color.LightGray,
+                    shape = RoundedCornerShape(4.dp)
+                )
+                .clip(shape = RoundedCornerShape(4.dp))
+        )
         Spacer(modifier = Modifier.height(8.dp))
-        Divider(
-            thickness = 1.dp,
+        Text(
+            text = "Justin Bieber, Bruno Mars,Coldplay, Dua Lipa, Imagikjkjkjkj",
+            style = MaterialTheme.typography.labelSmall,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 2,
             color = Color.LightGray
         )
     }
 }
 
 @Composable
-fun PileGraph() {
-    Box(
+fun RecommendationItem() {
+    Row(
         modifier = Modifier
-            .padding(top = 24.dp)
             .fillMaxWidth()
+            .height(56.dp)
+            .background(
+                color = Color.LightGray.copy(alpha = 0.4f),
+                shape = RoundedCornerShape(4.dp)
+            )
+            .clip(shape = RoundedCornerShape(4.dp)),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Box(
             modifier = Modifier
-                .size(350.dp)
-                .background(Color.Gray, shape = CircleShape)
-                .align(Alignment.Center)
+                .fillMaxHeight()
+                .width(56.dp)
+                .background(color = Color.LightGray)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = "Running test",
+            style = MaterialTheme.typography.bodySmall,
+            color = Color.White
         )
     }
 }
@@ -125,28 +196,49 @@ fun PileGraph() {
 private fun Header() {
     Row(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp),
+            .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Column(modifier = Modifier.weight(1f)) {
-            Text(
-                text = "Balance",
-                style = MaterialTheme.typography.bodySmall,
-                color = nautral_50
-            )
-
-            Text(
-                text = "73,276,931.28",
-                style = MaterialTheme.typography.headlineMedium,
-                color = Color.Black
-            )
-        }
-        Icon(
-            painter = painterResource(id = drawable.ic_baseline_add_circle_24),
-            contentDescription = "Add",
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(
+                    color = Color.LightGray,
+                    shape = CircleShape
+                )
         )
+        Spacer(modifier = Modifier.width(8.dp))
+        Tags()
+        Spacer(modifier = Modifier.height(16.dp))
     }
+}
+
+@Composable
+fun Tags() {
+    TagItem("All", true)
+    Spacer(modifier = Modifier.width(8.dp))
+    TagItem("Music", false)
+    Spacer(modifier = Modifier.width(8.dp))
+    TagItem("Podcasts", false)
+}
+
+@Composable
+fun TagItem(title: String, isSelected: Boolean) {
+    Text(
+        modifier = Modifier
+            .background(
+                color = if (isSelected)
+                    Color(0xFF4CAF50)
+                else
+                    Color(0xFF4B524C),
+                shape = CircleShape
+            )
+            .padding(horizontal = 22.dp)
+            .padding(vertical = 12.dp),
+        text = title,
+        style = MaterialTheme.typography.labelMedium,
+        color = if (isSelected) Color.Black else Color.White
+    )
 }
 
 @Preview
