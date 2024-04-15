98281036863b941987fe70804c17504949b8322d
Abdulrohim 'Him' Sama <a.rohim.sama@gmail.com>
2024-04-15 22:51:33 +0700

    Feat: Binding data from network with UI with search page for television screen

--- feature/search/src/main/java/com/him/sama/spotifycompose/search/screen/TelevisionSearchBody.kt
+++ feature/search/src/main/java/com/him/sama/spotifycompose/search/screen/TelevisionSearchBody.kt
@@ -1,18 +1,20 @@
 package com.him.sama.spotifycompose.search.screen
 
-import androidx.compose.foundation.background
+import SpotifyCircleLoading
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,15 +26,16 @@ import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.him.sama.spotifycompose.common.state.WindowSize
 import com.him.sama.spotifycompose.common.state.rememberWindowSize
-import com.him.sama.spotifycompose.common.ui.R
 import com.him.sama.spotifycompose.common.ui.preview.TelevisionPreview
 import com.him.sama.spotifycompose.common.ui.theme.AppTheme
 import com.him.sama.spotifycompose.common.ui.theme.background_color
-import com.him.sama.spotifycompose.common.ui.theme.title_text_color
+import com.him.sama.spotifycompose.search.CategoryModel
+import com.him.sama.spotifycompose.search.SearchPageModel
 import com.him.sama.spotifycompose.search.SearchViewState
 import com.him.sama.spotifycompose.search.component.BlackSearchBox
 
@@ -51,16 +54,24 @@ fun TelevisionSearchBody(windowSize: WindowSize, viewState: SearchViewState) {
         ) {
             BlackSearchBox()
             Spacer(modifier = Modifier.height(32.dp))
-            TelevisionSearchCategory(windowSize)
-            Spacer(modifier = Modifier.height(24.dp))
-            LazyVerticalGrid(
-                modifier = Modifier.fillMaxWidth(),
-                columns = GridCells.Fixed(5),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-            ) {
-                items(50) {
-                    TelevisionSearchCategoryItem()
+            if (viewState.isLoading) {
+                Box(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    SpotifyCircleLoading(modifier = Modifier.align(Alignment.Center))
+                }
+            } else {
+                TelevisionSearchCategory()
+                Spacer(modifier = Modifier.height(24.dp))
+                LazyVerticalGrid(
+                    modifier = Modifier.fillMaxWidth(),
+                    columns = GridCells.Fixed(5),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                ) {
+                    items(viewState.data.categories) {
+                        TelevisionSearchCategoryItem(it)
+                    }
                 }
             }
         }
@@ -68,32 +79,34 @@ fun TelevisionSearchBody(windowSize: WindowSize, viewState: SearchViewState) {
 }
 
 @Composable
-private fun TelevisionSearchCategoryItem() {
+private fun TelevisionSearchCategoryItem(categoryModel: CategoryModel) {
     Box(
         modifier = Modifier
             .size(140.dp)
             .clickable(onClick = {})
-            .background(Color.Black)
+            .clip(RoundedCornerShape(8.dp))
             .onFocusChanged {
-
             }
     ) {
+        AsyncImage(
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .size(140.dp),
+            model = categoryModel.image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
         Column(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .padding(bottom = 16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Icon(
-                modifier = Modifier.size(48.dp),
-                painter = painterResource(id = R.drawable.baseline_grid_view_24),
-                contentDescription = null,
-                tint = Color.White
-            )
+            Box(modifier = Modifier.size(48.dp))
             Spacer(modifier = Modifier.height(16.dp))
             Text(
-                text = "Music",
-                style = MaterialTheme.typography.bodyMedium,
+                text = categoryModel.title,
+                style = MaterialTheme.typography.titleMedium,
                 color = Color.White
             )
         }
@@ -101,7 +114,7 @@ private fun TelevisionSearchCategoryItem() {
 }
 
 @Composable
-fun TelevisionSearchCategory(windowSize: WindowSize) {
+fun TelevisionSearchCategory() {
     Column(
         modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
@@ -109,7 +122,7 @@ fun TelevisionSearchCategory(windowSize: WindowSize) {
         Text(
             text = "Genres & Moods",
             style = MaterialTheme.typography.titleLarge,
-            color = title_text_color
+            color = Color.White
         )
     }
 }
@@ -118,6 +131,34 @@ fun TelevisionSearchCategory(windowSize: WindowSize) {
 @Composable
 private fun PreviewSearchScreen() {
     AppTheme {
-        TelevisionSearchBody(rememberWindowSize(), SearchViewState.initial())
+        TelevisionSearchBody(
+            rememberWindowSize(),
+            SearchViewState.initial().copy(
+                data = SearchPageModel(
+                    categories = listOf(
+                        CategoryModel(
+                            title = "Comedy",
+                            bgColor = "#af2896",
+                            image = "https://i.scdn.co/image/ab6765630000ba8a77d267a5accb8911a92668e1"
+                        ),
+                        CategoryModel(
+                            title = "Comedy",
+                            bgColor = "#af2896",
+                            image = "https://i.scdn.co/image/ab6765630000ba8a77d267a5accb8911a92668e1"
+                        ),
+                        CategoryModel(
+                            title = "Comedy",
+                            bgColor = "#af2896",
+                            image = "https://i.scdn.co/image/ab6765630000ba8a77d267a5accb8911a92668e1"
+                        ),
+                        CategoryModel(
+                            title = "Comedy",
+                            bgColor = "#af2896",
+                            image = "https://i.scdn.co/image/ab6765630000ba8a77d267a5accb8911a92668e1"
+                        ),
+                    )
+                )
+            )
+        )
     }
 }
